689381688e6fe155fa604c4419e66c3976bd0247
SnooHD <snoohd@gmail.com>
2023-03-26 21:38:00 +0200

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         />
         <link
           rel="icon"
-          href="/favicon.ico"
+          href="/favicon.png"
         />
       </Head>
       <body>
